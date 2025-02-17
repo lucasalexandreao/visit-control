c6c6a093236ede05483584981cabbe2ea5f57452
Lucas Alexandre <lucasparaoif@gmail.com>
2025-02-17 16:34:41 -0300

    update views and js functions

---
+++ app/javascript/delete_button.js
@@ -0,0 +1,17 @@
+function handleDeleteConfirmation() {
+    console.log("ta rodando");
+    const deleteButtons = document.querySelectorAll('.delete-button');
+
+    deleteButtons.forEach(function(button) {
+        button.addEventListener("click", function(event) {
+            const confirmation = confirm('Você tem certeza de que deseja remover este item?');
+            if (!confirmation) {
+                event.preventDefault(); 
+            }
+        });
+    });
+}
+
+// Garante que a função seja chamada corretamente em mudanças de página com Turbo
+document.addEventListener("turbo:load", handleDeleteConfirmation);
+
